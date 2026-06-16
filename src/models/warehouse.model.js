534326d9687ad4e92ddae152f877c05a8a1c9647
shoaibejaz512 <shoaibejaz512@gmail.com>
2026-06-16 23:17:12 +0500

    make warehouse model

---
+++ src/models/warehouse.model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const warehouseSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Warehouse name is required"],
+      trim: true,
+    },
+
+    city: {
+      type: String,
+      required: [true, "City is required"],
+      trim: true,
+    },
+
+    address: {
+      type: String,
+      required: [true, "Address is required"],
+      trim: true,
+    },
+
+    contactNumber: {
+      type: String,
+      required: [true, "Contact number is required"],
+    },
+
+    manager: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model("Warehouse", warehouseSchema);
